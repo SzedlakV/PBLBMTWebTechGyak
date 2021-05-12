5b16de557ccc72be7be909fe0f43ca6def84547e
SzedlakV <vikiszedlak@gmail.com>
2021-05-12 09:13:03 +0200

    kiegeszites

---
+++ PBLBMT_0428/feldolgozo2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  
+  <title>Űrlap vezérlése új elemekkel </title>
+ </head>
+ <body style=background:url('kepek/hatter.jpg'); background-position:center; background-repeat: no repeat; background-attachment: fixed">
+  <font style="font-size:18pt">
+
+  <h2>Űrlap vezérlése új elemekkel </h2>
+
+     <?php
+
+ print ("Név: " . $_GET['nev'] . "<br>");
+
+ print ("PIN kód: " . $_GET['kod'] . "<br>");
+
+
+ ?>
+
+<a href="urlap3.html">vissza az űrlapra...</a>
+
+</font>
+
+ </body>
+</html>
